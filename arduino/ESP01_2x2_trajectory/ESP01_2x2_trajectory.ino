44261976bea5b6da07f750107451b008977a7e37
sarvesh patil <sbpatil97@gmail.com>
2022-11-17 23:20:30 -0500

    feedforward error

---
+++ arduino/ESP01_2x2_trajectory/ESP01_2x2_trajectory.ino
@@ -0,0 +1,158 @@
+#include "delta_trajectory.pb.h"
+#include "pb_common.h"
+#include "pb.h"
+#include "pb_encode.h"
+#include "pb_decode.h"
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include <Adafruit_ADS1X15.h>
+#include<math.h>
+
+#define NUM_MOTORS 12 
+#define MY_ID 16
+
+
+//################################## Feather MC and ADC Libraries INIT #####################3
+Adafruit_MotorShield MC0 = Adafruit_MotorShield(0x62);
+Adafruit_MotorShield MC1 = Adafruit_MotorShield(0x60);
+Adafruit_MotorShield MC2 = Adafruit_MotorShield(0x61);
+//
+Adafruit_DCMotor *MC0_M1 = MC0.getMotor(1);
+Adafruit_DCMotor *MC0_M2 = MC0.getMotor(2);
+Adafruit_DCMotor *MC0_M3 = MC0.getMotor(3);
+Adafruit_DCMotor *MC0_M4 = MC0.getMotor(4);
+Adafruit_DCMotor *MC1_M1 = MC1.getMotor(1);
+Adafruit_DCMotor *MC1_M2 = MC1.getMotor(2);
+Adafruit_DCMotor *MC1_M3 = MC1.getMotor(3);
+Adafruit_DCMotor *MC1_M4 = MC1.getMotor(4);
+Adafruit_DCMotor *MC2_M1 = MC2.getMotor(1);
+Adafruit_DCMotor *MC2_M2 = MC2.getMotor(2);
+Adafruit_DCMotor *MC2_M3 = MC2.getMotor(3);
+Adafruit_DCMotor *MC2_M4 = MC2.getMotor(4);
+//
+Adafruit_DCMotor* motors[NUM_MOTORS] = {MC0_M1,MC0_M2,MC1_M1,// 1st robot
+                                        MC1_M2,MC2_M1,MC2_M2,// 2nd robot
+                                        MC2_M3,MC2_M4,MC1_M3,// 3rd robot
+                                        MC1_M4,MC0_M3,MC0_M4,// 4th robot
+                                        };
+//Adafruit_DCMotor* motors[NUM_MOTORS] = {MC1_M4};
+
+
+Adafruit_ADS1015 ADC2;
+Adafruit_ADS1015 ADC1;
+Adafruit_ADS1015 ADC0;
+
+Adafruit_ADS1015* adcs[NUM_MOTORS] = {&ADC2, &ADC2, &ADC1,//1st robot
+                                      &ADC1, &ADC0, &ADC0,//2nd robot
+                                      &ADC0, &ADC0, &ADC1,//3rd robot
+                                      &ADC1, &ADC2, &ADC2,//4th robot
+                                      };
+
+//Adafruit_ADS1015* adcs[NUM_MOTORS] = {&ADC2};
+int channels[NUM_MOTORS] = {0,1,0,//1st robot
+                            1,0,1,//2nd robot
+                            2,3,2,//3rd robot
+                            3,2,3,//4th robot
+                            };
+//int channels[NUM_MOTORS] = {3};
+//##################################### GLOBAL VARIABLES ###################################3
+const int numChars = 3000;
+float pi = 3.1415926535;
+
+uint8_t input_cmd[numChars];
+bool newData = false;
+
+DeltaMessage message = DeltaMessage_init_zero;
+static boolean recvInProgress = false;
+int ndx = 0;
+int ndxx = 0;
+char startMarker = 0xA6;
+char confMarker = '~';
+char endMarker = 0xA7;
+int marker_count = 0;
+
+unsigned long current_arduino_time;
+unsigned long last_arduino_time;
+float time_elapsed;
+float total_time_elapsed;
+float exec_time;
+
+float joint_positions[12] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+float new_joint_positions[12] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+float trajectory[20][12];
+int traj_iter = 0;
+bool go = false;
+//float new_trajectory[20][12];
+
+float joint_errors[12] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+
+// We need last_joint_errors to compute PID control value for d. 
+float last_joint_errors[12] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+// We need total_joint_errors to compute PID control value for i. 
+float total_joint_errors[12] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+
+int motor_val[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+bool is_movement_done = false;
+bool skip_trajectory = false;
+
+//################################# SETUP AND LOOP FUNCTIONS ################################3
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial1.begin(115200);
+  while (!Serial1)
+    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+  
+  // set all the base dc motor control pins to outputs
+  MC0.begin();
+  MC1.begin();
+  MC2.begin();
+  // start all the ADCs
+  ADC2.begin(0x4A);
+  ADC1.begin(0x49);
+  ADC0.begin(0x48);
+
+  ADC2.setGain(GAIN_ONE);
+  ADC1.setGain(GAIN_ONE);
+  ADC0.setGain(GAIN_ONE);
+
+  // disable dc motors by setting their enable lines to low
+  for(int i=0; i<NUM_MOTORS; i++){
+    motors[i]->setSpeed(150);
+    motors[i]->run(RELEASE);
+    delay(10);
+  }
+  
+  Serial.println("started");
+  Serial1.println("AT+GMR");
+  delay(500);
+  Serial1.println("AT+CWMODE=1");
+  delay(500);
+  Serial1.println("AT+CIFSR");
+  delay(500);
+  Serial1.println("AT+CIPMUX=1");
+  delay(500);
+  Serial1.println("AT+CIPSERVER=1,80");
+  delay(500);
+  printIPAddr();
+  
+  readJointPositions();
+//  resetJoints();
+//  stop();
+}
+
+void loop() {
+//  Serial.println("HAKUNA MATATA");
+  // put your main code here, to run repeatedly:
+  recvWithStartEndMarkers();
+  if (newData == true) {
+    if (decodeNanopbData()){
+      if (!skip_trajectory){
+        updateTrajectory();
+      }
+    }
+    newData = false;
+    ndx = 0;
+  }
+  executeTrajectory();
+}
---
+++ python/test_feedforward.py
@@ -0,0 +1,91 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+from pyparsing import delimitedList
+from Prismatic_Delta import Prismatic_Delta
+from get_coords import RoboCoords
+import delta_trajectory_pb2
+from control_delta_arrays import DeltaArrayAgent
+from serial_robot_mapping import delta_comm_dict, inv_delta_comm_dict
+
+import telnetlib
+import socket
+
+# host = "192.168.1.31"
+robot_no = 16
+port = 80
+timeout = 100
+BUFFER_SIZE = 20
+
+# esp01 = telnetlib.Telnet(host, port, timeout)
+esp01 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+esp01.connect((inv_delta_comm_dict[robot_no], port))
+esp01.settimeout(0.1)
+
+# DDA = DeltaArrayAgent(esp01, delta_comm_dict[host])
+DDA = DeltaArrayAgent(esp01, robot_no)
+s_p = 1.5 #side length of the platform
+s_b = 4.3 #side length of the base
+l = 4.5 #length of leg attached to platform
+
+Delta = Prismatic_Delta(s_p, s_b, l)
+
+def create_joint_positions(val):
+    a = []
+    for i in range(4):
+        for j in range(3):
+            a.append(val[j])
+    return a
+
+def rotate(vector, angle, plot=False):
+        # Rotation from Delta Array axis to cartesian axis = 30 degrees. 
+        rot_matrix = np.array([[np.cos(angle), -np.sin(angle)],[np.sin(angle), np.cos(angle)]])
+        vector = vector@rot_matrix
+        return vector
+
+traj = [[0.0012993778983634763, -0.5235987755982988, 12.2001595082625],
+ [0.02431510295357648, -0.5235987755982988, 12.201572669850615],
+ [0.06893204933808428, -0.5235987755982988, 12.200691300958496],
+ [0.12952052727413474, -0.5235987755982988, 12.194693331215744],
+ [0.20229373935717573, -0.5235987755982988, 12.182002179493686],
+ [0.28424924572379173, -0.5235987755982988, 12.161551945387231],
+ [0.37261725396096557, -0.5235987755982988, 12.132388570460279],
+ [0.46465505894317227, -0.5235987755982988, 12.093503949164198],
+ [0.5575945233829457, -0.5235987755982988, 12.043793733693589],
+ [0.6486388965486422, -0.5235987755982988, 11.98206980781886],
+ [0.7349787840405556, -0.5235987755982988, 11.90710060182216],
+ [0.8138317323634067, -0.5235987755982988, 11.817682744535857],
+ [0.882523740316103, -0.5235987755982988, 11.712764618326295],
+ [0.9386325822351846, -0.5235987755982988, 11.591643976538865],
+ [0.9802039143078025, -0.5235987755982988, 11.454251659411957],
+ [1.0060356125564118, -0.5235987755982988, 11.30152024644078],
+ [1.0159882583156168, -0.5235987755982988, 11.135782130991075],
+ [1.0111924316483039, -0.5235987755982988, 10.960988514796776],
+ [0.9939720659460035, -0.5235987755982988, 10.782457510650541],
+ [0.9674393419836608, -0.5235987755982988, 10.606114772087544]]
+
+if __name__=="__main__":
+    delta_message = delta_trajectory_pb2.DeltaMessage()
+    delta_message.id = robot_no
+    delta_message.request_joint_pose = False
+    delta_message.request_done_state = False
+    delta_message.reset = False
+    for i in range(10000):
+        x = input("Go signal")
+        for j in range(20):
+            # ee_pts = [0,wave[j],10 + 2*wave[j]]
+            ee_pts = traj[j]
+            pts = Delta.IK(ee_pts)
+            pts = np.array(pts) * 0.01
+            pts = np.clip(pts,0.005,0.095)
+            jts = create_joint_positions(pts)
+            _ = [delta_message.trajectory.append(jts[i]) for i in range(12)]
+
+        serialized = delta_message.SerializeToString()
+        
+        print(len(serialized))
+        print(len(delta_message.trajectory))
+
+        esp01.send(b'\xa6~~'+ serialized + b'\xa7~~\r\n')
+        del delta_message.trajectory[:]
